21a6df543709e46a6329017d899f14d6776e466e
atene2001 <it2001@nate.com>
2018-10-23 10:34:37 +0900

    관리자 - 통계 - DAO / Service

--- ecobay/src/main/java/org/ecobay/admin/main/persistence/StatsAdminDAO.java
+++ ecobay/src/main/java/org/ecobay/admin/main/persistence/StatsAdminDAO.java
@@ -1,5 +1,13 @@
 package org.ecobay.admin.main.persistence;
 
-public class StatsAdminDAO {
+import java.util.List;
 
+import org.ecobay.admin.main.domain.StatsVO;
+
+public interface StatsAdminDAO {
+	public List<StatsVO> memberJoinDays(StatsVO svo) throws Exception;
+	public List<StatsVO> memberJoinDaysM(StatsVO svo) throws Exception;
+	public List<StatsVO> memberJoinDaysF(StatsVO svo) throws Exception;
+	public List<StatsVO> productPayState(StatsVO svo) throws Exception;
+	public List<StatsVO> productState(StatsVO svo) throws Exception;
 }
--- ecobay/src/main/java/org/ecobay/admin/main/service/StatsAdminService.java
+++ ecobay/src/main/java/org/ecobay/admin/main/service/StatsAdminService.java
@@ -1,5 +1,13 @@
 package org.ecobay.admin.main.service;
 
-public class StatsAdminService {
+import java.util.List;
 
+import org.ecobay.admin.main.domain.StatsVO;
+
+public interface StatsAdminService {
+	public List<StatsVO> memberJoinDays(StatsVO svo) throws Exception;
+	public List<StatsVO> memberJoinDaysM(StatsVO svo) throws Exception;
+	public List<StatsVO> memberJoinDaysF(StatsVO svo) throws Exception;
+	public List<StatsVO> productPayState(StatsVO svo) throws Exception;
+	public List<StatsVO> productState(StatsVO svo) throws Exception;
 }
